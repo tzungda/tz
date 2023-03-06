37a2f02d6c24fc81c36bec84bce45026a1f37c8d
tzungda <31838821+tzungda@users.noreply.github.com>
2023-03-05 20:03:31 -0800

    Update vm.c

--- vm.c
+++ vm.c
@@ -87,6 +87,20 @@ static InterpretResult run()
 
 InterpretResult interpret( const char* source )
 {
-	compile( source );
-	return INTERPRET_OK;
+	Chunk chunk;
+	initChunk( &chunk );
+	
+	if ( !compile( source, &chunk ) )
+	{
+		freeChunk( &chunk );
+		return INTERPRET_COMPLETE_ERROR;
+	}
+	
+	vm.chunk = &chunk;
+	vm.ip = vm.chunk->code;
+	
+	InterpretResult result = run();
+	
+	freeChunk( &chunk );
+	return result;
 }
